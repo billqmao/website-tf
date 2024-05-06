518a5629a9791a1b916e266b597f93fe4c7e3e51
Bill Mao <billqmao@gmail.com>
2024-05-06 16:29:19 -0700

    Add pages source

--- cloudflare.tf
+++ cloudflare.tf
@@ -4,10 +4,23 @@ resource "cloudflare_pages_project" "website" {
   name              = var.site_name
   production_branch = "main"
   build_config {
-    build_command       = "hugo"
-    destination_dir     = "public"
-    root_dir            = ""
+    build_command   = "hugo"
+    destination_dir = "public"
+    root_dir        = ""
   }
+  source {
+    type = "github"
+    config {
+      owner                         = "billqmao"
+      repo_name                     = var.repo_name
+      production_branch             = "master"
+      pr_comments_enabled           = true
+      deployments_enabled           = true
+      production_deployment_enabled = true
+      preview_branch_excludes       = ["master"]
+    }
+  }
+
 }
 
 #Custom domain
@@ -15,5 +28,6 @@ resource "cloudflare_pages_domain" "site_domain" {
   account_id   = var.account_id
   project_name = var.site_name
   domain       = var.domain
-  count = "${var.domain == "" ? 0 : 1}"
+  count        = var.domain == "" ? 0 : 1
 }
+
--- variables.tf
+++ variables.tf
@@ -13,3 +13,7 @@ variable "account_id" {
 variable "domain" {
   type = string
 }
+
+variable "repo_name" {
+    type = string
+}
